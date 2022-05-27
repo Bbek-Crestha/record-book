fc7a0619042eac14892fd5738313de7b7d420aa6
Bbek-Crestha <bcrestha07@gmail.com>
2022-05-27 23:07:35 +0545

    feat(client): create NotFound route to catch all incorrect paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Dashboard />} />
+
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import image from "../images/notFound.png";
+
+import { Button, Center, Heading, Image, Stack, Text } from "@chakra-ui/react";
+
+const NotFound = () => {
+	return (
+		<Center h="100vh">
+			<Stack align="center">
+				<Image src={image} alt="not found logo" w="350px" />
+
+				<Heading as="h3" size="md" textAlign="center">
+					Page Not Found
+				</Heading>
+
+				<Text fontSize="md" p={4} align="center">
+					Sorry, we couldn't find the page you are looking for. Please go back
+					to home page.
+				</Text>
+
+				<Link to="/">
+					<Button bg="primary" colorScheme="primary" size="md">
+						Go To Homepage
+					</Button>
+				</Link>
+			</Stack>
+		</Center>
+	);
+};
+
+export default NotFound;
