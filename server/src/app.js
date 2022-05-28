4aec8744c2bb9f5774da07656d45918ad6e3b4b5
Bbek-Crestha <bcrestha07@gmail.com>
2022-05-28 08:51:36 +0545

    feat(sever): add morgan for server logs and cors to avoid CORS error

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+app.use(cors());
 
 module.exports = app;
