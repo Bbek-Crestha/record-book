441bf4453d5c7e147c86ed4737c479dd122be5fb
Bbek-Crestha <bcrestha07@gmail.com>
2022-05-28 16:41:11 +0545

    feat(client): create and add navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Navbar = () => {
+	return (
+		<Box bg="blackAlpha.200">
+			<Heading>Navbar</Heading>
+		</Box>
+	);
+};
+
+export default Navbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
-
 import { Stack } from "@chakra-ui/react";
+
+import Navbar from "../components/Navbar";
 
 const Home = () => {
 	return (
-		<Stack direction="row">
+		<Stack
+			direction="row"
+			bgGradient="linear(to-b, green.200, red.200)"
+			h="100vh"
+		>
+			<Navbar />
 			<Outlet />
 		</Stack>
 	);
