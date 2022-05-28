6acfd3001e4c822d71c9af146b9a57d0aaf5f204
Bbek-Crestha <bcrestha07@gmail.com>
2022-05-28 16:39:15 +0545

    style(client): change background color to avoid flickering

--- client/src/components/CustomSpinner.js
+++ client/src/components/CustomSpinner.js
@@ -2,7 +2,7 @@ import { Stack, Spinner, Center } from "@chakra-ui/react";
 
 const CustomSpinner = () => {
 	return (
-		<Center h="100vh">
+		<Center h="100vh" bgGradient="linear(to-b, green.200, red.200)">
 			<Stack direction="row" spacing={4} align="center">
 				<Spinner size="xs" />
 				<Spinner size="sm" />
